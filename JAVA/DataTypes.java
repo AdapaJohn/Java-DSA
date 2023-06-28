a2fd9bce26bc5b0ee44be8e29647cdc4cb8d4e76
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2023-06-28 20:23:47 +0530

    Add files via upload

---
+++ JAVA/DataTypes.java
@@ -0,0 +1,20 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        byte b = 12;
+        int a = 100;
+        short c = 200;
+        long d = 987654321l;
+        float e = 200.06f;
+        double f = 2345.098d;
+        char ch = 'e';
+        // boolean t = ;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(ch);
+        // System.out.println(t);
+    }
+}
