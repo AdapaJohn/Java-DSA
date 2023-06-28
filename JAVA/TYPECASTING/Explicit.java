f9cbf62660d423ac9030a32826f81c044baa31d6
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2023-06-28 20:31:07 +0530

    Add files via upload

---
+++ JAVA/TYPECASTING/Explicit.java
@@ -0,0 +1,12 @@
+package TYPECASTING;
+
+public class Explicit {
+    public static void main(String[] args) {
+        float f = 20.3f;
+        int a = (int) f;
+        char ch = 'F';
+        int b = (int) ch;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
---
+++ JAVA/TYPECASTING/Implicit.java
@@ -0,0 +1,9 @@
+package TYPECASTING;
+
+public class Implicit {
+    public static void main(String[] args) {
+        int num = 546358510;
+        double data = num;
+        System.out.println("value is :"+ data);
+    }
+}
