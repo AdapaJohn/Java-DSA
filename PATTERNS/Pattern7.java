afb29b0425139b3481bdcd882c001b4c449d3d1e
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2023-10-27 22:28:10 +0530

    Add files via upload
         *    
       ***   
      *****  
     ******* 
    *********

---
+++ PATTERNS/Pattern7.java
@@ -0,0 +1,20 @@
+public class Pattern7 {
+    static void pattern7(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<2*i+1;j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int n=5;
+        pattern7(n);
+    }
+}
