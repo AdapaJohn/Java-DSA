75e54f3062c1a6c8805d61bdba4b345e9c73c856
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2023-10-27 22:26:09 +0530

    Add files via upload
    1 2 3 4 5 6 7 
    1 2 3 4 5 6 
    1 2 3 4 5 
    1 2 3 4 
    1 2 3 
    1 2 
    1

---
+++ PATTERNS/Pattern6.java
@@ -0,0 +1,14 @@
+public class Pattern6 {
+    static void pattern6(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int n=7;
+        pattern6(n);
+    }
+}
