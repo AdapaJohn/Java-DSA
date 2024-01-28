89a0cd79a94d1ae9ecb93fe00a7ae86684352ef4
unknown <adapajohnmu143@gmail.com>
2024-01-28 19:28:05 +0530

    If statement file added

---
+++ JAVA/DESCISIONMAKINGSTATEMENTS/Ifstatement.java
@@ -0,0 +1,15 @@
+package DESCISIONMAKINGSTATEMENTS;
+
+public class Ifstatement {
+    public static void main(String[] args) {
+        String str = "DSA";
+        int i =2;
+        if(i==2){
+            i++;
+            System.out.println(str);
+
+        }
+        System.out.println("Months="+i);
+
+    }
+}
