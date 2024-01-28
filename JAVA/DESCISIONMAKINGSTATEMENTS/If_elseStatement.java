81a65849047c65e7a4339580ec507b7a03de8e3b
unknown <adapajohnmu143@gmail.com>
2024-01-28 19:35:35 +0530

    If-else  statement file added

---
+++ JAVA/DESCISIONMAKINGSTATEMENTS/If_elseStatement.java
@@ -0,0 +1,12 @@
+package DESCISIONMAKINGSTATEMENTS;
+
+public class If_elseStatement {
+    public static void main(String[] args) {
+        int i=10;
+        if(i<5)
+        System.out.println("you are a kid");
+        else
+        System.out.println("you are a boy");
+        System.out.println("Only outside of if else working another line");
+    }
+}
