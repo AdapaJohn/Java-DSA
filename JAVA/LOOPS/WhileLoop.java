9cb506d34e8aacae18e423efa1f31c582926cca6
unknown <adapajohnmu143@gmail.com>
2024-01-29 21:32:23 +0530

    While Loop file added

---
+++ JAVA/LOOPS/WhileLoop.java
@@ -0,0 +1,11 @@
+package LOOPS;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i=0;
+        while (i<50) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
