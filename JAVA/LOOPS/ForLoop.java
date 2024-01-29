28a54de110f4a4ca82e6e02cf77de494eb1870ba
unknown <adapajohnmu143@gmail.com>
2024-01-29 21:29:53 +0530

    For Loop file added

---
+++ JAVA/LOOPS/ForLoop.java
@@ -0,0 +1,13 @@
+package LOOPS;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) { // Increasing
+            System.out.println(i);
+        }
+
+        for (int i = 10; i >= 1; i--) { // Decreasing
+            System.out.println(i);
+        }
+    }
+}
