75c0c3b62162d4ec86a5b7c306aaaa34a12c393d
unknown <adapajohnmu143@gmail.com>
2024-01-29 21:27:27 +0530

    ForEach Loop file added

---
+++ JAVA/LOOPS/ForEachLoop.java
@@ -0,0 +1,10 @@
+package LOOPS;
+
+public class ForEachLoop {
+    public static void main(String[] args) {
+        int arr[] = { 100, 99, 98, 97 };
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+}
