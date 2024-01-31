e6129b28817d1e9ce1e950aecdbee1b4a644d2e9
unknown <adapajohnmu143@gmail.com>
2024-01-31 22:28:24 +0530

    Class file added

---
+++ JAVA/OOP'S/CLASS/Class.java
@@ -0,0 +1,9 @@
+package OOP'S.CLASS;
+
+public class Class {
+    public static void main(String[] args) {
+        int i=10,j=5;
+        int sum=i+j;
+        System.out.println("Sum of i and j:"+sum);
+    }
+}
