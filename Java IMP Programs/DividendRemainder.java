24bb1368917d43d5caf173f24fdb5f33b0a79ec3
unknown <adapajohnmu143@gmail.com>
2024-02-04 22:50:42 +0530

    Dividend Remainder file added

---
+++ Java IMP Programs/DividendRemainder.java
@@ -0,0 +1,27 @@
+public class DividendRemainder {
+    public static void main(String[] args) {
+        int dividend = 27, divisor = 5;
+        int quotient = dividend / divisor;
+        float remainder = dividend % divisor;
+        System.out.println("the quotient is :" + quotient);
+        System.out.println("the remainder is :" + remainder);
+
+    }
+}
+/*
+ * public class QuotientRemainder {
+ * 
+ * public static void main(String[] args) {
+ * 
+ * int dividend = 25, divisor = 4;
+ * 
+ * int quotient = dividend / divisor;
+ * int remainder = dividend % divisor;
+ * 
+ * System.out.println("Quotient = " + quotient);
+ * System.out.println("Remainder = " + remainder);
+ * }
+ * }
+ */
+
+
