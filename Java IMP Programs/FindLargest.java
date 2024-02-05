d82b9fc85bd7aabe1fe0eaf1f68dc88fcd083681
unknown <adapajohnmu143@gmail.com>
2024-02-05 20:01:10 +0530

    Find Largest file updated

--- Java IMP Programs/FindLargest.java
+++ Java IMP Programs/FindLargest.java
@@ -1,25 +1,24 @@
 public class FindLargest {
     public static void main(String[] args) {
-        
-        float p1=23.5f,p2=24.56f,p3=32f,p4=288.768f;
-        if (p1>p2) {
-            if (p1>p3) {
+
+        float p1 = 23.5f, p2 = 24.56f, p3 = 32f, p4 = 288.768f;
+        if (p1 > p2) {
+            if (p1 > p3) {
                 System.out.println("p1 is largest number");
             } else {
                 System.out.println("p3 is largest number");
             }
-            
+
         } else {
-            if (p2>p3) {
-                if (p2>p4) {
+            if (p2 > p3) {
+                if (p2 > p4) {
                     System.out.println("p2 is Largest number");
                 } else {
                     System.out.println("p4 is Largest number");
                 }
-                else{
-                    System.out.println("p4 is largest number");
-                }
-            } 
+            } else {
+                System.out.println("p4 is largest number");
+            }
         }
     }
 }
