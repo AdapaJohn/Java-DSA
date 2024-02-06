64c6d52c2f6a1a1690a3d4cba80df93667581a47
unknown <adapajohnmu143@gmail.com>
2024-02-06 21:10:32 +0530

    Count vowels & Consonants file added

--- Java IMP Programs/CountVowelConsonants.java
+++ Java IMP Programs/CountVowelConsonants.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class CountVowelConsonants {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        int vowelsCount = 0;
+        int consonantsCount = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char ch = Character.toLowerCase(input.charAt(i));
+            if (ch >= 'a' && ch <= 'z') {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    vowelsCount++;
+                } else {
+                    consonantsCount++;
+                }
+            }
+        }
+        System.out.println("Number of vowels: " + vowelsCount);
+        System.out.println("Number of consonants: " + consonantsCount);
+    }
 }
