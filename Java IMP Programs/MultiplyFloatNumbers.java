7fb33210305e725f02fb708f21075a046f05f913
unknown <adapajohnmu143@gmail.com>
2024-02-06 19:21:45 +0530

    Multiply Float Numbers file added

---
+++ Java IMP Programs/MultiplyFloatNumbers.java
@@ -0,0 +1,22 @@
+public class MultiplyFloatNumbers {
+    public static void main(String[] args) {
+        float mn1=3546.45f;
+        float mn2=4755.3f;
+        float mn3=mn1*mn2;
+        System.out.println("multiplication  of two Numbers:"+ mn3);
+
+    }
+}
+/* public class MultiplyTwoNumbers {
+
+    public static void main(String[] args) {
+
+        float first = 1.5f;
+        float second = 2.0f;
+
+        float product = first * second;
+
+        System.out.println("The product is: " + product);
+    }
+}
+ */
