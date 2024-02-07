5bf48425554113d953595f9e9cd4977a0178565b
unknown <adapajohnmu143@gmail.com>
2024-02-07 19:22:53 +0530

    Positive Negative file added

---
+++ Java IMP Programs/PositiveNegative.java
@@ -0,0 +1,10 @@
+public class PositiveNegative {
+    public static void main(String[] args) {
+        long num = 340;
+        if (num<0) {
+            System.out.println(num+" is a Negative number");
+        } else {
+            System.out.println(num+" is a positive number");
+        }
+    }
+}
