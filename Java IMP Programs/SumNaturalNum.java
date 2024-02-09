065e66e535115a0bf0fd4944f17f29311f49035a
unknown <adapajohnmu143@gmail.com>
2024-02-09 18:24:33 +0530

    Sum Natural Num Order file added

---
+++ Java IMP Programs/SumNaturalNum.java
@@ -0,0 +1,14 @@
+public class SumNaturalNum {
+    public static void main(String[] args) {
+        int num=10,sum=0,i=0;
+        // for(int i=0;i<=num;i++){
+        //     sum+=num;
+        // }
+        while (i<=num) {
+            sum+=num;
+            i++;
+            System.out.println(sum);
+        }
+        System.out.println(sum);
+    }
+}
