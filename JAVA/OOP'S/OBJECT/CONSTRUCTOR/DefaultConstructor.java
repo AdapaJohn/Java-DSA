3b7e0a55ca3c46acf3e4f5d3cf79f1f3cbf5118d
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2024-02-12 22:42:12 +0530

    Default Constructor file added

---
+++ JAVA/OOP'S/OBJECT/CONSTRUCTOR/DefaultConstructor.java
@@ -0,0 +1,11 @@
+//package Java-DSA.JAVA.OOP'S.OBJECT.CONSTRUCTOR;
+
+public class DefaultConstructor {
+    DefaultConstructor() {
+        System.out.println("Bike is build");
+    }
+
+    public static void main(String[] args) {
+        DefaultConstructor bike = new DefaultConstructor();
+    }
+}
