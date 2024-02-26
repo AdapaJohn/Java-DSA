0d4e2ba339330643e09edf7e7299675e2c2c39b1
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2024-02-26 20:51:03 +0530

    Second Largest & Second Smallest Element file added
    Output:
    
    Second smallest is 2
    Second largest is 6

---
+++ Data Structures & Algorithms/1. Arrays/Easy/SecondLargest&SecondSmallestElement.java
@@ -0,0 +1,148 @@
+// Brute Force
+
+import java.io.*;
+import java.util.Arrays;
+class SecondLargestElement
+{
+static private void getElements(int[] arr, int n)
+{
+	if (n == 0 || n==1)
+	{
+		System.out.print(-1);
+		System.out.print(" ");
+		System.out.print(-1);
+		System.out.print("\n");
+	}
+	Arrays.sort(arr);
+	int small = arr[1];
+	int large = arr[n - 2];
+	System.out.println("Second smallest is "+small);
+	System.out.println("Second largest is "+large);
+}
+public static void main(String[] args)
+{
+	int[] arr = {1, 2, 4, 6, 7, 5};
+	int n = arr.length;
+	getElements(arr, n);
+}
+}
+
+//Time Complexity: O(NlogN), For sorting the array
+//Space Complexity: O(1)
+
+//Better Solution
+
+
+import java.io.*;
+import java.util.Arrays;
+class SecondLargestElement
+{
+static private void getElements(int[] arr, int n)
+{
+if (n == 0 || n==1)
+	{
+		System.out.print(-1);
+		System.out.print(" ");
+		System.out.print(-1);
+		System.out.print("\n");
+	}
+	int small = Integer.MAX_VALUE;
+	int second_small = Integer.MAX_VALUE;
+	int large = Integer.MIN_VALUE;
+	int second_large = Integer.MIN_VALUE;
+	int i;
+	for (i = 0;i < n;i++)
+	{
+		small = Math.min(small,arr[i]);
+		large = Math.max(large,arr[i]);
+	}
+	for (i = 0;i < n;i++)
+	{
+		if (arr[i] < second_small && arr[i] != small)
+		{
+			second_small = arr[i];
+		}
+		if (arr[i] > second_large && arr[i] != large)
+		{
+			second_large = arr[i];
+		}
+	}
+
+	System.out.println("Second smallest is "+second_small);
+	System.out.println("Second largest is "+second_large);
+}
+public static void main(String[] args)
+{
+	int[] arr = {1, 2, 4, 6, 7, 5};
+	int n = arr.length;
+	getElements(arr, n);
+}
+}
+
+//Time Complexity: O(N), We do two linear traversals in our array
+//Space Complexity: O(1)
+
+//Optimal Force
+
+import java.io.*;
+class SecondLargestElement
+{
+static private int secondSmallest(int[] arr, int n)
+{
+	if (n < 2)
+	{
+		return -1;
+	}
+	int small = Integer.MAX_VALUE;
+	int second_small = Integer.MAX_VALUE;
+	int i;
+	for (i = 0; i < n; i++)
+	{
+	   if (arr[i] < small)
+	   {
+		  second_small = small;
+		  small = arr[i];
+	   }
+	   else if (arr[i] < second_small && arr[i] != small)
+	   {
+		  second_small = arr[i];
+	   }
+	}
+   return second_small;
+}
+static private int secondLargest(int[] arr, int n)
+{
+	if(n<2)
+	return -1;
+	int large = Integer.MIN_VALUE;
+	int second_large = Integer.MIN_VALUE;
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		if (arr[i] > large)
+		{
+			second_large = large;
+			large = arr[i];
+		}
+
+		else if (arr[i] > second_large && arr[i] != large)
+		{
+			second_large = arr[i];
+		}
+	}
+	return second_large;
+}
+
+public static void main(String[] args)
+{
+	int[] arr = {1, 2, 4, 7, 7, 5};
+	int n = arr.length;
+		int sS = secondSmallest(arr, n);
+		int sL = secondLargest(arr, n);
+	System.out.println("Second smallest is "+sS);
+	System.out.println("Second largest is "+sL);
+}
+}
+
+//Time Complexity: O(N), Single-pass solution
+//Space Complexity: O(1)
