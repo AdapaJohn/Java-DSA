afcd3a0338afa70a4a3dd9a39a5e71728b5c7a74
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2024-02-28 22:05:37 +0530

    Remove Duplicates in-place from Sorted Array file added
    Output: The array after removing duplicate elements is 1 2 3

---
+++ Data Structures & Algorithms/1. Arrays/Easy/RemoveDuplicates.java
@@ -0,0 +1,55 @@
+//Brute force approach
+
+import java.util.*;
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        int arr[] = {1,1,2,2,2,3,3};
+        int k = removeDuplicates(arr);
+        System.out.println("The array after removing duplicate elements is ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    static int removeDuplicates(int[] arr) {
+        HashSet < Integer > set = new HashSet < > ();
+        for (int i = 0; i < arr.length; i++) {
+            set.add(arr[i]);
+        }
+        int k = set.size();
+        int j = 0;
+        for (int x: set) {
+            arr[j++] = x;
+        }
+        return k;
+    }
+}
+
+//Time complexity: O(n*log(n))+O(n)
+//Space Complexity: O(n) 
+
+//Optimal Approach
+
+import java.util.*;
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+       int arr[] = {1,1,2,2,2,3,3};
+        int k = removeDuplicates(arr);
+        System.out.println("The array after removing duplicate elements is ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    static int removeDuplicates(int[] arr) {
+        int i = 0;
+        for (int j = 1; j < arr.length; j++) {
+            if (arr[i] != arr[j]) {
+                i++;
+                arr[i] = arr[j];
+            }
+        }
+        return i + 1;
+    }
+}
+
+//Time Complexity: O(N)
+//Space Complexity: O(1)
