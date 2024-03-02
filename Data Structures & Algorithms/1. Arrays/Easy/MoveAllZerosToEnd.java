1e3f18f75a899ecd2df90c387e9579ac4b77130d
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2024-03-02 23:08:55 +0530

    Move all Zeros to the end of the array file added
    Output: 1 2 3 2 4 5 1 0 0 0

---
+++ Data Structures & Algorithms/1. Arrays/Easy/MoveAllZerosToEnd.java
@@ -0,0 +1,92 @@
+//BRUTE FORCE APPROACH
+
+import java.util.*;
+
+public class MoveAllZerosToEnd {
+    public static int[] moveZeros(int n, int []a) {
+        //temporary array:
+        ArrayList<Integer> temp = new ArrayList<>();
+        //copy non-zero elements
+        //from original -> temp array:
+        for (int i = 0; i < n; i++) {
+            if (a[i] != 0)
+                temp.add(a[i]);
+        }
+
+        // number of non-zero elements.
+        int nz = temp.size();
+
+        //copy elements from temp
+        //fill first nz fields of
+        //original array:
+        for (int i = 0; i < nz; i++) {
+            a[i] = temp.get(i);
+        }
+
+        //fill rest of the cells with 0:
+        for (int i = nz; i < n; i++) {
+            a[i] = 0;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 0, 2, 3, 2, 0, 0, 4, 5, 1};
+        int n = 10;
+        int[] ans = moveZeros(n, arr);
+        for (int i = 0; i < n; i++) {
+            System.out.print(ans[i] + " ");
+        }
+        System.out.println("");
+    }
+}
+
+//Time Complexity: O(N) + O(X) + O(N-X) ~ O(2*N), where N = total no. of elements,X = no. of non-zero elements, and N-X = total no. of zeros.
+//Space Complexity: O(N)
+
+//OPTIMAL APPROACH
+
+import java.util.*;
+
+public class MoveAllZerosToEnd {
+    public static int[] moveZeros(int n, int []a) {
+        int j = -1;
+        //place the pointer j:
+        for (int i = 0; i < n; i++) {
+            if (a[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+
+        //no non-zero elements:
+        if (j == -1) return a;
+
+        //Move the pointers i and j
+        //and swap accordingly:
+        for (int i = j + 1; i < n; i++) {
+            if (a[i] != 0) {
+                //swap a[i] & a[j]:
+                int tmp = a[i];
+                a[i] = a[j];
+                a[j] = tmp;
+                j++;
+            }
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 0, 2, 3, 2, 0, 0, 4, 5, 1};
+        int n = 10;
+        int[] ans = moveZeros(n, arr);
+        for (int i = 0; i < n; i++) {
+            System.out.print(ans[i] + " ");
+        }
+        System.out.println("");
+    }
+}
+
+//Time Complexity: O(N), N = size of the array.
+//Reason: We have used 2 loops and using those loops, we are basically traversing the array once.
+//Space Complexity: O(1) as we are not using any extra space to solve this problem.
