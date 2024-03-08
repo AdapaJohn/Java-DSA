22679e23434f0b94c5cc80fb62b2384bf4787ad6
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2024-03-08 10:02:22 +0530

    The number that appears once, and the other numbers twice file added
    Output: The single element is: 4

---
+++ Data Structures & Algorithms/1. Arrays/Easy/NumberThatAppearsOnceAndOtherNumbersTwice.java
@@ -0,0 +1,151 @@
+//Naive Approach(Brute-force approach): 
+
+import java.util.*;
+
+public class NumberThatAppearsOnceAndOtherNumbersTwice {
+    public static int getSingleElement(int []arr) {
+        // Size of the array:
+        int n = arr.length;
+
+        //Run a loop for selecting elements:
+        for (int i = 0; i < n; i++) {
+            int num = arr[i]; // selected element
+            int cnt = 0;
+
+            //find the occurrence using linear search:
+            for (int j = 0; j < n; j++) {
+                if (arr[j] == num)
+                    cnt++;
+            }
+
+            // if the occurrence is 1 return ans:
+            if (cnt == 1) return num;
+        }
+
+        //This line will never execute
+        //if the array contains a single element.
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        int[] arr = {4, 1, 2, 1, 2};
+        int ans = getSingleElement(arr);
+        System.out.println("The single element is: " + ans);
+
+    }
+}
+
+//Time Complexity: O(N2), where N = size of the given array.
+//Space Complexity: O(1) as we are not using any extra space.
+
+//Better Approach 1(Using Hashing): 
+
+import java.util.*;
+
+public class NumberThatAppearsOnceAndOtherNumbersTwice {
+    public static int getSingleElement(int []arr) {
+        //size of the array:
+        int n = arr.length;
+
+        // Find the maximum element:
+        int maxi = arr[0];
+        for (int i = 0; i < n; i++) {
+            maxi = Math.max(maxi, arr[i]);
+        }
+
+        // Declare hash array of size maxi+1
+        // And hash the given array:
+        int[] hash = new int[maxi + 1];
+        for (int i = 0; i < n; i++) {
+            hash[arr[i]]++;
+        }
+
+        //Find the single element and return the answer:
+        for (int i = 0; i < n; i++) {
+            if (hash[arr[i]] == 1)
+                return arr[i];
+        }
+
+        //This line will never execute
+        //if the array contains a single element.
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        int[] arr = {4, 1, 2, 1, 2};
+        int ans = getSingleElement(arr);
+        System.out.println("The single element is: " + ans);
+
+    }
+}
+
+//Time Complexity: O(N)+O(N)+O(N), where N = size of the array
+//Space Complexity: O(maxElement+1) where maxElement = the maximum element of the array.
+
+//Better Approach 2(Hashing using the map data structure): 
+
+import java.util.*;
+
+public class NumberThatAppearsOnceAndOtherNumbersTwice {
+    public static int getSingleElement(int []arr) {
+        //size of the array:
+        int n = arr.length;
+
+        // Declare the hashmap.
+        // And hash the given array:
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int value = mpp.getOrDefault(arr[i], 0);
+            mpp.put(arr[i], value + 1);
+        }
+
+        //Find the single element and return the answer:
+        for (Map.Entry<Integer, Integer> it : mpp.entrySet()) {
+            if (it.getValue() == 1) {
+                return it.getKey();
+            }
+        }
+
+        //This line will never execute
+        //if the array contains a single element.
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        int[] arr = {4, 1, 2, 1, 2};
+        int ans = getSingleElement(arr);
+        System.out.println("The single element is: " + ans);
+
+    }
+}
+
+//Time Complexity: O(N*logM) + O(M), where M = size of the map i.e. M = (N/2)+1. N = size of the array.
+//Space Complexity: O(M) as we are using a map data structure. Here M = size of the map i.e. M = (N/2)+1.
+
+//Optimal Approach(Using XOR): 
+
+import java.util.*;
+
+public class NumberThatAppearsOnceAndOtherNumbersTwice {
+    public static int getSingleElement(int []arr) {
+        //size of the array:
+        int n = arr.length;
+
+        // XOR all the elements:
+        int xorr = 0;
+        for (int i = 0; i < n; i++) {
+            xorr = xorr ^ arr[i];
+        }
+        return xorr;
+    }
+
+    public static void main(String args[]) {
+        int[] arr = {4, 1, 2, 1, 2};
+        int ans = getSingleElement(arr);
+        System.out.println("The single element is: " + ans);
+
+    }
+}
+
+//Time Complexity: O(N), where N = size of the array.
+//Space Complexity: O(1) as we are not using any extra space.
