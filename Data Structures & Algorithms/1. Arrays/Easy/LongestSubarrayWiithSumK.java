2ae9e0dbce375384cdf55a3bbc9d1425dedfce64
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2024-03-09 10:32:10 +0530

    Longest Subarray with given Sum K(Positives) file added
    Output: The length of the longest subarray is: 3

---
+++ Data Structures & Algorithms/1. Arrays/Easy/LongestSubarrayWiithSumK.java
@@ -0,0 +1,161 @@
+//Naive Approach (Brute-force approach): 
+
+import java.util.*;
+
+public class LongestSubarrayWiithSumK {
+    public static int getLongestSubarray(int []a, long k) {
+        int n = a.length; // size of the array.
+
+        int len = 0;
+        for (int i = 0; i < n; i++) { // starting index
+            for (int j = i; j < n; j++) { // ending index
+                // add all the elements of
+                // subarray = a[i...j]:
+                long s = 0;
+                for (int K = i; K <= j; K++) {
+                    s += a[K];
+                }
+
+                if (s == k)
+                    len = Math.max(len, j - i + 1);
+            }
+        }
+        return len;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {2, 3, 5, 1, 9};
+        long k = 10;
+        int len = getLongestSubarray(a, k);
+        System.out.println("The length of the longest subarray is: " + len);
+    }
+}
+
+//Time Complexity: O(N3) approx., where N = size of the array.
+//Space Complexity: O(1) as we are not using any extra space.
+
+//Optimizing the Naive Approach (Using two loops): 
+
+import java.util.*;
+
+public class LongestSubarrayWiithSumK {
+    public static int getLongestSubarray(int []a, long k) {
+        int n = a.length; // size of the array.
+
+        int len = 0;
+        for (int i = 0; i < n; i++) { // starting index
+            long s = 0; // Sum variable
+            for (int j = i; j < n; j++) { // ending index
+                // add the current element to
+                // the subarray a[i...j-1]:
+                s += a[j];
+
+                if (s == k)
+                    len = Math.max(len, j - i + 1);
+            }
+        }
+        return len;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {2, 3, 5, 1, 9};
+        long k = 10;
+        int len = getLongestSubarray(a, k);
+        System.out.println("The length of the longest subarray is: " + len);
+    }
+}
+
+//Time Complexity: O(N2) approx., where N = size of the array.
+//Space Complexity: O(1) as we are not using any extra space.
+
+//Better Approach(Using Hashing):
+
+import java.util.*;
+
+public class LongestSubarrayWiithSumK {
+    public static int getLongestSubarray(int []a, long k) {
+        int n = a.length; // size of the array.
+
+        Map<Long, Integer> preSumMap = new HashMap<>();
+        long sum = 0;
+        int maxLen = 0;
+        for (int i = 0; i < n; i++) {
+            //calculate the prefix sum till index i:
+            sum += a[i];
+
+            // if the sum = k, update the maxLen:
+            if (sum == k) {
+                maxLen = Math.max(maxLen, i + 1);
+            }
+
+            // calculate the sum of remaining part i.e. x-k:
+            long rem = sum - k;
+
+            //Calculate the length and update maxLen:
+            if (preSumMap.containsKey(rem)) {
+                int len = i - preSumMap.get(rem);
+                maxLen = Math.max(maxLen, len);
+            }
+
+            //Finally, update the map checking the conditions:
+            if (!preSumMap.containsKey(sum)) {
+                preSumMap.put(sum, i);
+            }
+        }
+
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {2, 3, 5, 1, 9};
+        long k = 10;
+        int len = getLongestSubarray(a, k);
+        System.out.println("The length of the longest subarray is: " + len);
+    }
+}
+
+//Time Complexity: O(N) or O(N*logN) depending on which map data structure we are using, where N = size of the array.
+//Space Complexity: O(N) as we are using a map data structure.
+
+//Optimal Approach (Using 2 pointers): 
+
+import java.util.*;
+
+public class LongestSubarrayWiithSumK {
+    public static int getLongestSubarray(int []a, long k) {
+        int n = a.length; // size of the array.
+
+        int left = 0, right = 0; // 2 pointers
+        long sum = a[0];
+        int maxLen = 0;
+        while (right < n) {
+            // if sum > k, reduce the subarray from left
+            // until sum becomes less or equal to k:
+            while (left <= right && sum > k) {
+                sum -= a[left];
+                left++;
+            }
+
+            // if sum = k, update the maxLen i.e. answer:
+            if (sum == k) {
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+
+            // Move forward thw right pointer:
+            right++;
+            if (right < n) sum += a[right];
+        }
+
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {2, 3, 5, 1, 9};
+        long k = 10;
+        int len = getLongestSubarray(a, k);
+        System.out.println("The length of the longest subarray is: " + len);
+    }
+}
+
+//Time Complexity: O(2*N), where N = size of the given array.
+//Space Complexity: O(1) as we are not using any extra space.
