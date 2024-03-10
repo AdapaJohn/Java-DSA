c4f54cab5762a2f5b36a7527b29162caa648a44a
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2024-03-10 16:12:47 +0530

    Two Sum : Check if a pair with given sum exists in Array file added
    Output: This is the answer for variant 1: YES
    Output: This is the answer for variant 1: YES
    for same to brute,better and optimal approaches

---
+++ Data Structures & Algorithms/1. Arrays/Medium/2Sum.java
@@ -0,0 +1,153 @@
+//Naive Approach(Brute-force approach): 
+
+//code variant 1:
+import java.util.*;
+
+public class 2Sum {
+    public static String twoSum(int n, int []arr, int target) {
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] + arr[j] == target) return "YES";
+            }
+        }
+        return "NO";
+    }
+
+    public static void main(String args[]) {
+        int n = 5;
+        int[] arr = {2, 6, 5, 8, 11};
+        int target = 14;
+        String ans = twoSum(n, arr, target);
+        System.out.println("This is the answer for variant 1: " + ans);
+    }
+
+}
+
+//code variant 2:
+import java.util.*;
+
+public class 2Sum {
+    public static int[] twoSum(int n, int []arr, int target) {
+        int[] ans = new int[2];
+        ans[0] = ans[1] = -1;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] + arr[j] == target) {
+                    ans[0] = i;
+                    ans[1] = j;
+                    return ans;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String args[]) {
+        int n = 5;
+        int[] arr = {2, 6, 5, 8, 11};
+        int target = 14;
+        int[] ans = twoSum(n, arr, target);
+        System.out.println("This is the answer for variant 2: [" + ans[0] + ", "
+                           + ans[1] + "]");
+    }
+
+}
+
+//Time Complexity: O(N2), where N = size of the array.
+//Space Complexity: O(1) as we are not using any extra space.
+
+//Better Approach(using Hashing): 
+
+//code variant 1:
+import java.util.*;
+
+public class 2Sum {
+    public static String twoSum(int n, int []arr, int target) {
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int num = arr[i];
+            int moreNeeded = target - num;
+            if (mpp.containsKey(moreNeeded)) {
+                return "YES";
+            }
+
+            mpp.put(arr[i], i);
+        }
+        return "NO";
+    }
+
+    public static void main(String args[]) {
+        int n = 5;
+        int[] arr = {2, 6, 5, 8, 11};
+        int target = 14;
+        String ans = twoSum(n, arr, target);
+        System.out.println("This is the answer for variant 1: " + ans);
+    }
+}
+
+//code variant 2:
+import java.util.*;
+
+public class 2Sum {
+    public static int[] twoSum(int n, int []arr, int target) {
+        int[] ans = new int[2];
+        ans[0] = ans[1] = -1;
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int num = arr[i];
+            int moreNeeded = target - num;
+            if (mpp.containsKey(moreNeeded)) {
+                ans[0] = mpp.get(moreNeeded);
+                ans[1] = i;
+                return ans;
+            }
+
+            mpp.put(arr[i], i);
+        }
+        return ans;
+    }
+
+    public static void main(String args[]) {
+        int n = 5;
+        int[] arr = {2, 6, 5, 8, 11};
+        int target = 14;
+        int[] ans = twoSum(n, arr, target);
+        System.out.println("This is the answer for variant 2: [" + ans[0] + ", "
+                           + ans[1] + "]");
+
+    }
+}
+
+//Time Complexity: O(N), where N = size of the array.
+//Space Complexity: O(N) as we use the map data structure.
+
+//Optimized Approach(using two-pointer): 
+
+import java.util.*;
+
+public class 2Sum {
+    public static String twoSum(int n, int []arr, int target) {
+        Arrays.sort(arr);
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+            if (sum == target) {
+                return "YES";
+            } else if (sum < target) left++;
+            else right--;
+        }
+        return "NO";
+    }
+
+    public static void main(String args[]) {
+        int n = 5;
+        int[] arr = {2, 6, 5, 8, 11};
+        int target = 14;
+        String ans = twoSum(n, arr, target);
+        System.out.println("This is the answer for variant 1: " + ans);
+
+    }
+} 
+
+//Time Complexity: O(N) + O(N*logN), where N = size of the array.
+//Space Complexity: O(1) as we are not using any extra space.
