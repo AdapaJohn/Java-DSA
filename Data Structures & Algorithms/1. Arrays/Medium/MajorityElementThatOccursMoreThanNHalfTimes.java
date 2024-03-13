0aa1e94dc2db43a2920f57a73b0970f4aef61aba
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2024-03-13 20:40:22 +0530

    Find the Majority Element that occurs more than N/2 times file added

---
+++ Data Structures & Algorithms/1. Arrays/Medium/MajorityElementThatOccursMoreThanNHalfTimes.java
@@ -0,0 +1,119 @@
+//Naive Approach(BRUTE) :
+
+import java.util.*;
+
+public class MajorityElementThatOccursMoreThanNHalfTimes {
+    public static int majorityElement(int []v) {
+        //size of the given array:
+        int n = v.length;
+
+        for (int i = 0; i < n; i++) {
+            //selected element is v[i]
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                // counting the frequency of v[i]
+                if (v[j] == v[i]) {
+                    cnt++;
+                }
+            }
+
+            // check if frquency is greater than n/2:
+            if (cnt > (n / 2))
+                return v[i];
+        }
+
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        int[] arr = {2, 2, 1, 1, 1, 2, 2};
+        int ans = majorityElement(arr);
+        System.out.println("The majority element is: " + ans);
+
+    }
+
+} 
+
+//Time Complexity: O(N2), where N = size of the given array. Reason: For every element of the array the inner loop runs for N times. And there are N elements in the array. So, the total time complexity is O(N2).
+//Space Complexity: O(1) as we use no extra space.
+
+//Better Approach:
+
+import java.util.*;
+
+public class MajorityElementThatOccursMoreThanNHalfTimes {
+    public static int majorityElement(int []v) {
+        //size of the given array:
+        int n = v.length;
+
+        //declaring a map:
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+
+        //storing the elements with its occurnce:
+        for (int i = 0; i < n; i++) {
+            int value = mpp.getOrDefault(v[i], 0);
+            mpp.put(v[i], value + 1);
+        }
+
+        //searching for the majority element:
+        for (Map.Entry<Integer, Integer> it : mpp.entrySet()) {
+            if (it.getValue() > (n / 2)) {
+                return it.getKey();
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        int[] arr = {2, 2, 1, 1, 1, 2, 2};
+        int ans = majorityElement(arr);
+        System.out.println("The majority element is: " + ans);
+
+    }
+}
+
+//Time Complexity: O(N*logN) + O(N), where N = size of the given array.
+//Space Complexity: O(N) as we are using a map data structure.
+
+//Optimal Approach: Moore’s Voting Algorithm:
+
+import java.util.*;
+
+public class MajorityElementThatOccursMoreThanNHalfTimes {
+    public static int majorityElement(int []v) {
+        //size of the given array:
+        int n = v.length;
+        int cnt = 0; // count
+        int el = 0; // Element
+
+        //applying the algorithm:
+        for (int i = 0; i < n; i++) {
+            if (cnt == 0) {
+                cnt = 1;
+                el = v[i];
+            } else if (el == v[i]) cnt++;
+            else cnt--;
+        }
+
+        //checking if the stored element
+        // is the majority element:
+        int cnt1 = 0;
+        for (int i = 0; i < n; i++) {
+            if (v[i] == el) cnt1++;
+        }
+
+        if (cnt1 > (n / 2)) return el;
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        int[] arr = {2, 2, 1, 1, 1, 2, 2};
+        int ans = majorityElement(arr);
+        System.out.println("The majority element is: " + ans);
+
+    }
+}
+
+//Time Complexity: O(N) + O(N), where N = size of the given array.
+//Space Complexity: O(1) as we are not using any extra space.
