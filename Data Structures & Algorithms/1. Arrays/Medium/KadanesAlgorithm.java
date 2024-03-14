0ddfef65dc3e9d7e3018e0a7f97660de2bfd9329
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2024-03-14 21:25:35 +0530

    Kadane’s Algorithm : Maximum Subarray Sum in an Array file added
    Output: The maximum subarray sum is: 6
    
    Output for follow-up question:
    The subarray is: [4 -1 2 1 ]
    The maximum subarray sum is: 6

---
+++ Data Structures & Algorithms/1. Arrays/Medium/KadanesAlgorithm.java
@@ -0,0 +1,173 @@
+//Brute Force Approach: 
+
+import java.util.*;
+
+public class KadanesAlgorithm {
+    public static int maxSubarraySum(int[] arr, int n) {
+        int maxi = Integer.MIN_VALUE; // maximum sum
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                // subarray = arr[i.....j]
+                int sum = 0;
+
+                //add all the elements of subarray:
+                for (int k = i; k <= j; k++) {
+                    sum += arr[k];
+                }
+
+                maxi = Math.max(maxi, sum);
+            }
+        }
+
+        return maxi;
+    }
+
+    public static void main(String args[]) {
+        int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int n = arr.length;
+        int maxSum = maxSubarraySum(arr, n);
+        System.out.println("The maximum subarray sum is: " + maxSum);
+
+    }
+}
+
+//Time Complexity: O(N3), where N = size of the array.
+//Space Complexity: O(1) as we are not using any extra space.
+
+//Better Approach:
+
+import java.util.*;
+
+public class KadanesAlgorithm {
+    public static int maxSubarraySum(int[] arr, int n) {
+        int maxi = Integer.MIN_VALUE; // maximum sum
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                // current subarray = arr[i.....j]
+
+                //add the current element arr[j]
+                // to the sum i.e. sum of arr[i...j-1]
+                sum += arr[j];
+
+                maxi = Math.max(maxi, sum); // getting the maximum
+            }
+        }
+
+        return maxi;
+    }
+
+    public static void main(String args[]) {
+        int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int n = arr.length;
+        int maxSum = maxSubarraySum(arr, n);
+        System.out.println("The maximum subarray sum is: " + maxSum);
+
+    }
+}
+
+//Time Complexity: O(N2), where N = size of the array.
+//Space Complexity: O(1) as we are not using any extra space.
+
+//Optimal Approach:
+
+
+import java.util.*;
+
+public class KadanesAlgorithm {
+    public static long maxSubarraySum(int[] arr, int n) {
+        long maxi = Long.MIN_VALUE; // maximum sum
+        long sum = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            sum += arr[i];
+
+            if (sum > maxi) {
+                maxi = sum;
+            }
+
+            // If sum < 0: discard the sum calculated
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        // To consider the sum of the empty subarray
+        // uncomment the following check:
+
+        //if (maxi < 0) maxi = 0;
+
+        return maxi;
+    }
+
+    public static void main(String args[]) {
+        int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int n = arr.length;
+        long maxSum = maxSubarraySum(arr, n);
+        System.out.println("The maximum subarray sum is: " + maxSum);
+
+    }
+}
+
+//Time Complexity: O(N), where N = size of the array.
+//Space Complexity: O(1) as we are not using any extra space.
+
+//Follow-up question:
+
+import java.util.*;
+
+public class KadanesAlgorithm {
+    public static long maxSubarraySum(int[] arr, int n) {
+        long maxi = Long.MIN_VALUE; // maximum sum
+        long sum = 0;
+
+        int start = 0;
+        int ansStart = -1, ansEnd = -1;
+        for (int i = 0; i < n; i++) {
+
+            if (sum == 0) start = i; // starting index
+
+            sum += arr[i];
+
+            if (sum > maxi) {
+                maxi = sum;
+
+                ansStart = start;
+                ansEnd = i;
+            }
+
+            // If sum < 0: discard the sum calculated
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        //printing the subarray:
+        System.out.print("The subarray is: [");
+        for (int i = ansStart; i <= ansEnd; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.print("]n");
+
+        // To consider the sum of the empty subarray
+        // uncomment the following check:
+
+        //if (maxi < 0) maxi = 0;
+
+        return maxi;
+    }
+
+    public static void main(String args[]) {
+        int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int n = arr.length;
+        long maxSum = maxSubarraySum(arr, n);
+        System.out.println("The maximum subarray sum is: " + maxSum);
+
+    }
+}
+
+//Time Complexity: O(N), where N = size of the array.
+//Space Complexity: O(1) as we are not using any extra space.
