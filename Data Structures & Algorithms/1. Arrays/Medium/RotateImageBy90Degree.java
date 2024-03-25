de26fd1c50699f8a111e0669b1d3a83cd836c5d1
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2024-03-25 10:21:35 +0530

    Rotate Image by 90 degree file added

---
+++ Data Structures & Algorithms/1. Arrays/Medium/RotateImageBy90Degree.java
@@ -0,0 +1,71 @@
+//Brute Force Approach :
+
+import java.util.*;
+class RotateImageBy90Degree {
+    static int[][] rotate(int[][] matrix) {
+        int n = matrix.length;
+        int rotated[][] = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                rotated[j][n - i - 1] = matrix[i][j];
+            }
+        }
+        return rotated;
+    }
+
+    public static void main(String args[]) {
+        int arr[][] =  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int rotated[][] = rotate(arr);
+        System.out.println("Rotated Image");
+        for (int i = 0; i < rotated.length; i++) {
+            for (int j = 0; j < rotated.length; j++) {
+                System.out.print(rotated[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
+//Time Complexity: O(N*N) to linearly iterate and put it into some other matrix.
+//Space Complexity: O(N*N) to copy it into some other matrix.
+
+//Optimal Approach:
+
+import java.util.*;
+class RotateImageBy90Degree {
+    static void rotate(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length / 2; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = temp;
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[][] =  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        rotate(arr);
+        System.out.println("Rotated Image");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
+//Time Complexity: O(N*N) + O(N*N).One O(N*N) is for transposing the matrix and the other is for reversing the matrix.
+//Space Complexity: O(1).
