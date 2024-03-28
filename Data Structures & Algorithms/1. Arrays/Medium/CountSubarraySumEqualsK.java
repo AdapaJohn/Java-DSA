c787d7e3cb14ffe0d1b60f7283665b1a2cb3d394
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2024-03-28 16:54:23 +0530

    Count Subarray sum Equals K file added
    Output: The number of subarrays is: 2

---
+++ Data Structures & Algorithms/1. Arrays/Medium/CountSubarraySumEqualsK.java
@@ -0,0 +1,109 @@
+//Brute Force Approach:
+
+import java.util.*;
+
+public class CountSubarraySumEqualsK {
+    public static int findAllSubarraysWithGivenSum(int arr[], int k) {
+        int n = arr.length; // size of the given array.
+        int cnt = 0; // Number of subarrays:
+
+        for (int i = 0 ; i < n; i++) { // starting index i
+            for (int j = i; j < n; j++) { // ending index j
+
+                // calculate the sum of subarray [i...j]
+                int sum = 0;
+                for (int K = i; K <= j; K++)
+                    sum += arr[K];
+
+                // Increase the count if sum == k:
+                if (sum == k)
+                    cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 2, 4};
+        int k = 6;
+        int cnt = findAllSubarraysWithGivenSum(arr, k);
+        System.out.println("The number of subarrays is: " + cnt);
+    }
+}
+
+//Time Complexity: O(N3), where N = size of the array.
+//Space Complexity: O(1) as we are not using any extra space.
+
+//Better Approach
+
+import java.util.*;
+
+public class CountSubarraySumEqualsK {
+    public static int findAllSubarraysWithGivenSum(int arr[], int k) {
+        int n = arr.length; // size of the given array.
+        int cnt = 0; // Number of subarrays:
+
+        for (int i = 0 ; i < n; i++) { // starting index i
+            int sum = 0;
+            for (int j = i; j < n; j++) { // ending index j
+                // calculate the sum of subarray [i...j]
+                // sum of [i..j-1] + arr[j]
+                sum += arr[j];
+
+                // Increase the count if sum == k:
+                if (sum == k)
+                    cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 2, 4};
+        int k = 6;
+        int cnt = findAllSubarraysWithGivenSum(arr, k);
+        System.out.println("The number of subarrays is: " + cnt);
+    }
+}  
+
+//Time Complexity: O(N2), where N = size of the array.
+//Space Complexity: O(1) as we are not using any extra space.
+
+//Optimal Approach
+
+import java.util.*;
+
+public class CountSubarraySumEqualsK {
+    public static int findAllSubarraysWithGivenSum(int arr[], int k) {
+        int n = arr.length; // size of the given array.
+        Map mpp = new HashMap();
+        int preSum = 0, cnt = 0;
+
+        mpp.put(0, 1); // Setting 0 in the map.
+        for (int i = 0; i < n; i++) {
+            // add current element to prefix Sum:
+            preSum += arr[i];
+
+            // Calculate x-k:
+            int remove = preSum - k;
+
+            // Add the number of subarrays to be removed:
+            cnt += mpp.getOrDefault(remove, 0);
+
+            // Update the count of prefix sum
+            // in the map.
+            mpp.put(preSum, mpp.getOrDefault(preSum, 0) + 1);
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 2, 4};
+        int k = 6;
+        int cnt = findAllSubarraysWithGivenSum(arr, k);
+        System.out.println("The number of subarrays is: " + cnt);
+    }
+}
+
+//Time Complexity: O(N) or O(N*logN) depending on which map data structure we are using, where N = size of the array.
+//Space Complexity: O(N) as we are using a map data structure.
