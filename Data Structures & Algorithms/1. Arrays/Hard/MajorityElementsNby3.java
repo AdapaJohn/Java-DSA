bdd1bd5093e0681a6cfaa6ae0e3bbc8f88abab67
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2024-03-31 19:07:58 +0530

    Majority Elements(&gt;N/3 times) | Find the elements that appears more than N/3 times in the array file added
    Output: The majority elements are: 11 33

---
+++ Data Structures & Algorithms/1. Arrays/Hard/MajorityElementsNby3.java
@@ -0,0 +1,155 @@
+//Brute Force Approach:
+
+import java.util.*;
+
+public class MajorityElementsNby3 {
+    public static List<Integer> majorityElement(int []v) {
+        int n = v.length; //size of the array
+        List<Integer> ls = new ArrayList<>(); // list of answers
+
+        for (int i = 0; i < n; i++) {
+            //selected element is v[i]:
+            // Checking if v[i] is not already
+            // a part of the answer:
+            if (ls.size() == 0 || ls.get(0) != v[i]) {
+                int cnt = 0;
+                for (int j = 0; j < n; j++) {
+                    // counting the frequency of v[i]
+                    if (v[j] == v[i]) {
+                        cnt++;
+                    }
+                }
+
+                // check if frquency is greater than n/3:
+                if (cnt > (n / 3))
+                    ls.add(v[i]);
+            }
+
+            if (ls.size() == 2) break;
+        }
+
+        return ls;
+    }
+
+    public static void main(String args[]) {
+        int[] arr = {11, 33, 33, 11, 33, 11};
+        List<Integer> ans = majorityElement(arr);
+        System.out.print("The majority elements are: ");
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.print(ans.get(i) + " ");
+        }
+        System.out.println();
+    }
+}
+
+//Time Complexity: O(N2), where N = size of the given array.
+//Reason: For every element of the array the inner loop runs for N times. And there are N elements in the array. So, the total time complexity is O(N2).
+//Space Complexity: O(1) as we are using a list that stores a maximum of 2 elements. The space used is so small that it can be considered constant.
+
+//Better Approach (Using Hashing): 
+
+import java.util.*;
+
+public class MajorityElementsNby3 {
+    public static List<Integer> majorityElement(int []v) {
+        int n = v.length; //size of the array
+        List<Integer> ls = new ArrayList<>(); // list of answers
+
+        //declaring a map:
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+
+        // least occurrence of the majority element:
+        int mini = (int)(n / 3) + 1;
+
+        //storing the elements with its occurnce:
+        for (int i = 0; i < n; i++) {
+            int value = mpp.getOrDefault(v[i], 0);
+            mpp.put(v[i], value + 1);
+
+            //checking if v[i] is
+            // the majority element:
+            if (mpp.get(v[i]) == mini) {
+                ls.add(v[i]);
+            }
+            if (ls.size() == 2) break;
+        }
+
+        return ls;
+    }
+
+    public static void main(String args[]) {
+        int[] arr = {11, 33, 33, 11, 33, 11};
+        List<Integer> ans = majorityElement(arr);
+        System.out.print("The majority elements are: ");
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.print(ans.get(i) + " ");
+        }
+        System.out.println();
+    }
+}
+
+//Time Complexity: O(N*logN), where N = size of the given array.
+//Reason: We are using a map data structure. Insertion in the map takes logN time. And we are doing it for N elements. So, it results in the first term O(N*logN). If we use unordered_map instead, the first term will be O(N) for the best and average case and for the worst case, it will be O(N2).
+//Space Complexity: O(N) as we are using a map data structure. We are also using a list that stores a maximum of 2 elements. That space used is so small that it can be considered constant.
+
+//Optimal Approach (Extended Boyer Moore’s Voting Algorithm): 
+
+import java.util.*;
+
+public class MajorityElementsNby3 {
+    public static List<Integer> majorityElement(int []v) {
+        int n = v.length; //size of the array
+
+        int cnt1 = 0, cnt2 = 0; // counts
+        int el1 = Integer.MIN_VALUE; // element 1
+        int el2 = Integer.MIN_VALUE; // element 2
+
+        // applying the Extended Boyer Moore's Voting Algorithm:
+        for (int i = 0; i < n; i++) {
+            if (cnt1 == 0 && el2 != v[i]) {
+                cnt1 = 1;
+                el1 = v[i];
+            } else if (cnt2 == 0 && el1 != v[i]) {
+                cnt2 = 1;
+                el2 = v[i];
+            } else if (v[i] == el1) cnt1++;
+            else if (v[i] == el2) cnt2++;
+            else {
+                cnt1--; cnt2--;
+            }
+        }
+
+        List<Integer> ls = new ArrayList<>(); // list of answers
+
+        // Manually check if the stored elements in
+        // el1 and el2 are the majority elements:
+        cnt1 = 0; cnt2 = 0;
+        for (int i = 0; i < n; i++) {
+            if (v[i] == el1) cnt1++;
+            if (v[i] == el2) cnt2++;
+        }
+
+        int mini = (int)(n / 3) + 1;
+        if (cnt1 >= mini) ls.add(el1);
+        if (cnt2 >= mini) ls.add(el2);
+
+        // Uncomment the following line
+        // if it is told to sort the answer array:
+        //Collections.sort(ls); //TC --> O(2*log2) ~ O(1);
+
+        return ls;
+    }
+
+    public static void main(String args[]) {
+        int[] arr = {11, 33, 33, 11, 33, 11};
+        List<Integer> ans = majorityElement(arr);
+        System.out.print("The majority elements are: ");
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.print(ans.get(i) + " ");
+        }
+        System.out.println();
+    }
+}
+
+//Time Complexity: O(N) + O(N), where N = size of the given array.Reason: The first O(N) is to calculate the counts and find the expected majority elements. The second one is to check if the calculated elements are the majority ones or not.
+//Space Complexity: O(1) as we are only using a list that stores a maximum of 2 elements. The space used is so small that it can be considered constant.
