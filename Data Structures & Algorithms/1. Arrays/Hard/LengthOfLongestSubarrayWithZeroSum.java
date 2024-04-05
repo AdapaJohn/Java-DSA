0052e53379555e8d8cfbd4b07d01932c8965b207
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2024-04-05 23:09:50 +0530

    Length of the longest subarray with zero Sum file added
    Output: 5
    Output: 3

---
+++ Data Structures & Algorithms/1. Arrays/Hard/LengthOfLongestSubarrayWithZeroSum.java
@@ -0,0 +1,62 @@
+//Brute Force Approach
+
+import java.util.*;
+
+public class LengthOfLongestSubarrayWithZeroSum {
+static int solve(int[] a){
+	int  max = 0;
+	for(int i = 0; i < a.length; ++i){
+		int sum = 0;
+		for(int j = i; j < a.length; ++j){
+			sum += a[j];
+			if(sum == 0){
+				max = Math.max(max, j-i+1);
+			}
+		}
+	}
+	return max;
+   }
+
+    public static void main(String args[]) 
+    { 
+        int a[] = {9, -3, 3, -1, 6, -5};
+        System.out.println(solve(a));
+    } 
+}
+
+//Time Complexity: O(N^2) as we have two loops for traversal
+//Space Complexity: O(1) as we aren’t using any extra space.
+
+//Optimal Approach
+
+int maxLen(int A[], int n)
+    {
+        // Your code here
+        HashMap<Integer, Integer> mpp = new HashMap<Integer, Integer>();
+
+        int maxi = 0;
+        int sum = 0; 
+
+        for(int i = 0;i<n;i++) {
+
+            sum += A[i]; 
+
+            if(sum == 0) {
+                maxi = i + 1; 
+            }
+            else {
+                if(mpp.get(sum) != null) {
+
+                    maxi = Math.max(maxi, i - mpp.get(sum)); 
+                }
+                else {
+
+                    mpp.put(sum, i); 
+                }
+            }
+        }
+        return maxi; 
+    }
+
+//Time Complexity: O(N), as we are traversing the array only once
+//Space Complexity: O(N), in the worst case we would insert all array elements prefix sum into our hashmap
