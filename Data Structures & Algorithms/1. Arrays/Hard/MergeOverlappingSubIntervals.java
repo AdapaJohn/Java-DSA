5aac9f87e61ffdec1fa3115652e3c239bc52ac71
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2024-04-09 10:37:05 +0530

    Merge Overlapping Sub-intervals file added
    Output: [[1,6],[8,10],[15,18]]

---
+++ Data Structures & Algorithms/1. Arrays/Hard/MergeOverlappingSubIntervals.java
@@ -0,0 +1,100 @@
+//Brute Force Approach:
+
+import java.util.*;
+
+public class MergeOverlappingSubIntervals {
+
+    public static List<List<Integer>> mergeOverlappingIntervals(int[][] arr) {
+        int n = arr.length; // size of the array
+        //sort the given intervals:
+        Arrays.sort(arr, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return a[0] - b[0];
+            }
+        });
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) { // select an interval:
+            int start = arr[i][0];
+            int end = arr[i][1];
+
+            //Skip all the merged intervals:
+            if (!ans.isEmpty() && end <= ans.get(ans.size() - 1).get(1)) {
+                continue;
+            }
+
+            //check the rest of the intervals:
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j][0] <= end) {
+                    end = Math.max(end, arr[j][1]);
+                } else {
+                    break;
+                }
+            }
+            ans.add(Arrays.asList(start, end));
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {{1, 3}, {8, 10}, {2, 6}, {15, 18}};
+        List<List<Integer>> ans = mergeOverlappingIntervals(arr);
+        System.out.print("The merged intervals are: \n");
+        for (List<Integer> it : ans) {
+            System.out.print("[" + it.get(0) + ", " + it.get(1) + "] ");
+        }
+        System.out.println();
+    }
+
+}
+
+//Time Complexity: O(N*logN) + O(2*N), where N = the size of the given array.Reason: Sorting the given array takes  O(N*logN) time complexity. Now, after that, we are using 2 loops i and j. But while using loop i, we skip all the intervals that are merged with loop j. So, we can conclude that every interval is roughly visited twice(roughly, once for checking or skipping and once for merging). So, the time complexity will be 2*N instead of N2.
+//Space Complexity: O(N), as we are using an answer list to store the merged intervals. Except for the answer array, we are not using any extra space.
+
+//Optimal Approach: 
+
+import java.util.*;
+
+public class MergeOverlappingSubIntervals {
+
+    public static List<List<Integer>> mergeOverlappingIntervals(int[][] arr) {
+        int n = arr.length; // size of the array
+        //sort the given intervals:
+        Arrays.sort(arr, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return a[0] - b[0];
+            }
+        });
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            // if the current interval does not
+            // lie in the last interval:
+            if (ans.isEmpty() || arr[i][0] > ans.get(ans.size() - 1).get(1)) {
+                ans.add(Arrays.asList(arr[i][0], arr[i][1]));
+            }
+            // if the current interval
+            // lies in the last interval:
+            else {
+                ans.get(ans.size() - 1).set(1,
+                                            Math.max(ans.get(ans.size() - 1).get(1), arr[i][1]));
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {{1, 3}, {8, 10}, {2, 6}, {15, 18}};
+        List<List<Integer>> ans = mergeOverlappingIntervals(arr);
+        System.out.print("The merged intervals are: \n");
+        for (List<Integer> it : ans) {
+            System.out.print("[" + it.get(0) + ", " + it.get(1) + "] ");
+        }
+        System.out.println();
+    }
+}
+
+//Time Complexity: O(N*logN) + O(N), where N = the size of the given array.Reason: Sorting the given array takes  O(N*logN) time complexity. Now, after that, we are just using a single loop that runs for N times. So, the time complexity will be O(N).
+//Space Complexity: O(N), as we are using an answer list to store the merged intervals. Except for the answer array, we are not using any extra space.
