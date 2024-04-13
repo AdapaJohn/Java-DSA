d9f373156022eb1451a9b20d1934ab44de72f8a9
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2024-04-13 11:12:48 +0530

    Count inversions in an array file added
    Output: The number of inversions is: 10

---
+++ Data Structures & Algorithms/1. Arrays/Hard/CountInversions.java
@@ -0,0 +1,102 @@
+//Naive Approach (Brute force): 
+
+import java.util.*;
+
+public class CountInversions {
+
+    public static int numberOfInversions(int[] a, int n) {
+        // Count the number of pairs:
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (a[i] > a[j]) cnt++;
+            }
+        }
+        return cnt;
+    }
+
+
+    public static void main(String[] args) {
+        int[] a = {5, 4, 3, 2, 1};
+        int n = 5;
+        int cnt = numberOfInversions(a, n);
+        System.out.println("The number of inversions is: " + cnt);
+    }
+}
+
+//Time Complexity: O(N2), where N = size of the given array.Reason: We are using nested loops here and those two loops roughly run for N times.
+//Space Complexity: O(1) as we are not using any extra space to solve this problem.
+
+//Optimal Approach:
+
+import java.util.*;
+
+public class CountInversions {
+
+    private static int merge(int[] arr, int low, int mid, int high) {
+        ArrayList<Integer> temp = new ArrayList<>(); // temporary array
+        int left = low;      // starting index of left half of arr
+        int right = mid + 1;   // starting index of right half of arr
+
+        //Modification 1: cnt variable to count the pairs:
+        int cnt = 0;
+
+        //storing elements in the temporary array in a sorted manner//
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left]);
+                left++;
+            } else {
+                temp.add(arr[right]);
+                cnt += (mid - left + 1); //Modification 2
+                right++;
+            }
+        }
+
+        // if elements on the left half are still left //
+
+        while (left <= mid) {
+            temp.add(arr[left]);
+            left++;
+        }
+
+        //  if elements on the right half are still left //
+        while (right <= high) {
+            temp.add(arr[right]);
+            right++;
+        }
+
+        // transfering all elements from temporary to arr //
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp.get(i - low);
+        }
+        return cnt; // Modification 3
+    }
+
+    public static int mergeSort(int[] arr, int low, int high) {
+        int cnt = 0;
+        if (low >= high) return cnt;
+        int mid = (low + high) / 2 ;
+        cnt += mergeSort(arr, low, mid);  // left half
+        cnt += mergeSort(arr, mid + 1, high); // right half
+        cnt += merge(arr, low, mid, high);  // merging sorted halves
+        return cnt;
+    }
+
+    public static int numberOfInversions(int[] a, int n) {
+        // Count the number of pairs:
+        return mergeSort(a, 0, n - 1);
+    }
+
+
+    public static void main(String[] args) {
+        int[] a = {5, 4, 3, 2, 1};
+        int n = 5;
+        int cnt = numberOfInversions(a, n);
+        System.out.println("The number of inversions are: " + cnt);
+    }
+}
+
+//Time Complexity: O(N*logN), where N = size of the given array.Reason: We are not changing the merge sort algorithm except by adding a variable to it. So, the time complexity is as same as the merge sort.
+//Space Complexity: O(N), as in the merge sort We use a temporary array to store elements in sorted order.
