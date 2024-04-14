f81ea2d400d5438a0e7b6cce9fa7e85d8ddd17e9
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2024-04-14 11:12:30 +0530

    Count Reverse Pairs file added
    Output: The number of reverse pair is: 3

---
+++ Data Structures & Algorithms/1. Arrays/Hard/CountReversePairs.java
@@ -0,0 +1,111 @@
+//Naive Approach (Brute-force): 
+
+import java.util.*;
+
+public class CountReversePairs {
+
+    public static int countPairs(int[] a, int n) {
+
+        // Count the number of pairs:
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (a[i] > 2 * a[j])
+                    cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    public static int team(int[] skill, int n) {
+        return countPairs(skill, n);
+    }
+
+    public static void main(String[] args) {
+        int[] a = {4, 1, 2, 3, 1};
+        int n = 5;
+        int cnt = team(a, n);
+        System.out.println("The number of reverse pair is: " + cnt);
+    }
+}  
+
+//Time Complexity: O(N2), where N = size of the given array.Reason: We are using nested loops here and those two loops roughly run for N times.
+//Space Complexity: O(1) as we are not using any extra space to solve this problem.
+
+//Optimal Approach: 
+
+import java.util.*;
+
+public class CountReversePairs {
+
+    private static void merge(int[] arr, int low, int mid, int high) {
+        ArrayList<Integer> temp = new ArrayList<>(); // temporary array
+        int left = low;      // starting index of left half of arr
+        int right = mid + 1;   // starting index of right half of arr
+
+        //storing elements in the temporary array in a sorted manner//
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left]);
+                left++;
+            } else {
+                temp.add(arr[right]);
+                right++;
+            }
+        }
+
+        // if elements on the left half are still left //
+
+        while (left <= mid) {
+            temp.add(arr[left]);
+            left++;
+        }
+
+        //  if elements on the right half are still left //
+        while (right <= high) {
+            temp.add(arr[right]);
+            right++;
+        }
+
+        // transfering all elements from temporary to arr //
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp.get(i - low);
+        }
+    }
+
+    public static int countPairs(int[] arr, int low, int mid, int high) {
+        int right = mid + 1;
+        int cnt = 0;
+        for (int i = low; i <= mid; i++) {
+            while (right <= high && arr[i] > 2 * arr[right]) right++;
+            cnt += (right - (mid + 1));
+        }
+        return cnt;
+    }
+
+    public static int mergeSort(int[] arr, int low, int high) {
+        int cnt = 0;
+        if (low >= high) return cnt;
+        int mid = (low + high) / 2 ;
+        cnt += mergeSort(arr, low, mid);  // left half
+        cnt += mergeSort(arr, mid + 1, high); // right half
+        cnt += countPairs(arr, low, mid, high); //Modification
+        merge(arr, low, mid, high);  // merging sorted halves
+        return cnt;
+    }
+
+    public static int team(int[] skill, int n) {
+        return mergeSort(skill, 0, n - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] a = {4, 1, 2, 3, 1};
+        int n = 5;
+        int cnt = team(a, n);
+        System.out.println("The number of reverse pair is: " + cnt);
+    }
+}
+
+//Time Complexity: O(2N*logN), where N = size of the given array.Reason: Inside the mergeSort() we call merge() and countPairs() except mergeSort() itself. Now, inside the function countPairs(), though we are running a nested loop, we are actually iterating the left half once and the right half once in total. That is why, the time complexity is O(N). And the merge() function also takes O(N). The mergeSort() takes O(logN) time complexity. Therefore, the overall time complexity will be O(logN * (N+N)) = O(2N*logN).
+//Space Complexity: O(N), as in the merge sort We use a temporary array to store elements in sorted order.
