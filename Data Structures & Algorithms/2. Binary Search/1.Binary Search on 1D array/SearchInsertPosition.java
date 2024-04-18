627a999221afd92a6cd1d5d67ac04e99d34c0670
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2024-04-18 21:47:14 +0530

    Search Insert Position file added
    Output: The index is: 3

---
+++ Data Structures & Algorithms/2. Binary Search/1.Binary Search on 1D array/SearchInsertPosition.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class SearchInsertPosition {
+
+    public static int searchInsert(int [] arr, int x) {
+        int n = arr.length; // size of the array
+        int low = 0, high = n - 1;
+        int ans = n;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            // maybe an answer
+            if (arr[mid] >= x) {
+                ans = mid;
+                //look for smaller index on the left
+                high = mid - 1;
+            } else {
+                low = mid + 1; // look on the right
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 7};
+        int x = 6;
+        int ind = searchInsert(arr, x);
+        System.out.println("The index is: " + ind);
+    }
+}
+
+//Time Complexity: O(logN), where N = size of the given array. Reason: We are basically using the Binary Search algorithm.
+//Space Complexity: O(1) as we are using no extra space.
