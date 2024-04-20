5f505bd15a17e313f54cd4fb9e5aeb83603f2b35
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2024-04-20 23:42:11 +0530

    Floor and Ceil in Sorted Array file added
    Output: The floor and ceil are: 4 7

---
+++ Data Structures & Algorithms/2. Binary Search/1.Binary Search on 1D array/FloorAndCeil.java
@@ -0,0 +1,60 @@
+//What is the floor of x?
+//The floor of x is the largest element in the array which is smaller than or equal to x( i.e. largest element in the array <= x).
+
+//What is the ceiling of x?
+//The ceiling of x is the smallest element in the array greater than or equal to x( i.e. smallest element in the array >= x).
+
+//import java.util.*;
+
+public class FloorAndCeil {
+    static int findFloor(int[] arr, int n, int x) {
+        int low = 0, high = n - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            // maybe an answer
+            if (arr[mid] <= x) {
+                ans = arr[mid];
+                //look for smaller index on the left
+                low = mid + 1;
+            } else {
+                high = mid - 1; // look on the right
+            }
+        }
+        return ans;
+    }
+
+    static int findCeil(int[] arr, int n, int x) {
+        int low = 0, high = n - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            // maybe an answer
+            if (arr[mid] >= x) {
+                ans = arr[mid];
+                //look for smaller index on the left
+                high = mid - 1;
+            } else {
+                low = mid + 1; // look on the right
+            }
+        }
+        return ans;
+    }
+    public static int[] getFloorAndCeil(int[] arr, int n, int x) {
+        int f = findFloor(arr, n, x);
+        int c = findCeil(arr, n, x);
+        return new int[] {f, c};
+    }
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 4, 7, 8, 10};
+        int n = 6, x = 5;
+        int[] ans = getFloorAndCeil(arr, n, x);
+        System.out.println("The floor and ceil are: " + ans[0]
+                           + " " + ans[1]);
+    }
+} 
+
+//Time Complexity: O(logN), where N = size of the given array. Reason: We are basically using the Binary Search algorithm.
+//Space Complexity: O(1) as we are using no extra space.
