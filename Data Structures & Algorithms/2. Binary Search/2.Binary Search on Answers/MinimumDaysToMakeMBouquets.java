9e776bcce1750d16a61a6c7b7f01f65ac6c9a2f0
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2024-05-03 23:45:11 +0530

    Minimum days to make M bouquets file added
    Output: We can make bouquets on day 12.

---
+++ Data Structures & Algorithms/2. Binary Search/2.Binary Search on Answers/MinimumDaysToMakeMBouquets.java
@@ -0,0 +1,115 @@
+//Naive Approach(Brute force): 
+
+import java.util.*;
+
+public class MinimumDaysToMakeMBouquets {
+    public static boolean possible(int[] arr, int day, int m, int k) {
+        int n = arr.length; // Size of the array
+        int cnt = 0;
+        int noOfB = 0;
+        // Count the number of bouquets:
+        for (int i = 0; i < n; i++) {
+            if (arr[i] <= day) {
+                cnt++;
+            } else {
+                noOfB += (cnt / k);
+                cnt = 0;
+            }
+        }
+        noOfB += (cnt / k);
+        return noOfB >= m;
+    }
+
+    public static int roseGarden(int[] arr, int k, int m) {
+        long val = (long) m * k;
+        int n = arr.length; // Size of the array
+        if (val > n) return -1; // Impossible case.
+        // Find maximum and minimum:
+        int mini = Integer.MAX_VALUE, maxi = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            mini = Math.min(mini, arr[i]);
+            maxi = Math.max(maxi, arr[i]);
+        }
+
+        for (int i = mini; i <= maxi; i++) {
+            if (possible(arr, i, m, k))
+                return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {7, 7, 7, 7, 13, 11, 12, 7};
+        int k = 3;
+        int m = 2;
+        int ans = roseGarden(arr, k, m);
+        if (ans == -1)
+            System.out.println("We cannot make m bouquets.");
+        else
+            System.out.println("We can make bouquets on day " + ans);
+    }
+}
+
+//Time Complexity: O((max(arr[])-min(arr[])+1) * N), where {max(arr[]) -> maximum element of the array, min(arr[]) -> minimum element of the array, N = size of the array}. Reason: We are running a loop to check our answers that are in the range of [min(arr[]), max(arr[])]. For every possible answer, we will call the possible() function. Inside the possible() function, we are traversing the entire array, which results in O(N).
+//Space Complexity: O(1) as we are not using any extra space to solve this problem.
+
+//Optimal Approach(Using Binary Search): 
+
+import java.util.*;
+
+public class MinimumDaysToMakeMBouquets {
+    public static boolean possible(int[] arr, int day, int m, int k) {
+        int n = arr.length; // Size of the array
+        int cnt = 0;
+        int noOfB = 0;
+        // Count the number of bouquets:
+        for (int i = 0; i < n; i++) {
+            if (arr[i] <= day) {
+                cnt++;
+            } else {
+                noOfB += (cnt / k);
+                cnt = 0;
+            }
+        }
+        noOfB += (cnt / k);
+        return noOfB >= m;
+    }
+
+    public static int roseGarden(int[] arr, int k, int m) {
+        long val = (long) m * k;
+        int n = arr.length; // Size of the array
+        if (val > n) return -1; // Impossible case.
+        // Find maximum and minimum:
+        int mini = Integer.MAX_VALUE, maxi = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            mini = Math.min(mini, arr[i]);
+            maxi = Math.max(maxi, arr[i]);
+        }
+
+        // Apply binary search:
+        int low = mini, high = maxi;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (possible(arr, mid, m, k)) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {7, 7, 7, 7, 13, 11, 12, 7};
+        int k = 3;
+        int m = 2;
+        int ans = roseGarden(arr, k, m);
+        if (ans == -1)
+            System.out.println("We cannot make m bouquets.");
+        else
+            System.out.println("We can make bouquets on day " + ans);
+    }
+}
+
+//Time Complexity: O(log(max(arr[])-min(arr[])+1) * N), where {max(arr[]) -> maximum element of the array, min(arr[]) -> minimum element of the array, N = size of the array}. Reason: We are applying binary search on our answers that are in the range of [min(arr[]), max(arr[])]. For every possible answer ‘mid’, we will call the possible() function. Inside the possible() function, we are traversing the entire array, which results in O(N).
+//Space Complexity: O(1) as we are not using any extra space to solve this problem.
