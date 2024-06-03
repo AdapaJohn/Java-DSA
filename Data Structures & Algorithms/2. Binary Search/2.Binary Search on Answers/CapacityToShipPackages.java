6d8554d9a4c15cdeca76dcb9d01bd535eb8300eb
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2024-06-03 17:10:57 +0530

    Capacity to Ship Packages within D Days file added
    Output:The minimum capacity should be: 9.

---
+++ Data Structures & Algorithms/2. Binary Search/2.Binary Search on Answers/CapacityToShipPackages.java
@@ -0,0 +1,100 @@
+//Brute Force Approach:
+
+import java.util.*;
+
+public class CapacityToShipPackages {
+    public static int findDays(int[] weights, int cap) {
+        int days = 1; //First day.
+        int load = 0;
+        int n = weights.length; //size of array.
+        for (int i = 0; i < n; i++) {
+            if (load + weights[i] > cap) {
+                days += 1; //move to next day
+                load = weights[i]; //load the weight.
+            } else {
+                //load the weight on the same day.
+                load += weights[i];
+            }
+        }
+        return days;
+    }
+
+    public static int leastWeightCapacity(int[] weights, int d) {
+        //Find the maximum and the summation:
+        int maxi = Integer.MIN_VALUE, sum = 0;
+        for (int i = 0; i < weights.length; i++) {
+            sum += weights[i];
+            maxi = Math.max(maxi, weights[i]);
+        }
+
+        for (int i = maxi; i <= sum; i++) {
+            if (findDays(weights, i) <= d) {
+                return i;
+            }
+        }
+        //dummy return statement:
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] weights = {5, 4, 5, 2, 3, 4, 5, 6};
+        int d = 5;
+        int ans = leastWeightCapacity(weights, d);
+        System.out.println("The minimum capacity should be: " + ans);
+    }
+}
+
+//Time Complexity: O(N * (sum(weights[]) - max(weights[]) + 1)), where sum(weights[]) = summation of all the weights, max(weights[]) = maximum of all the weights, N = size of the weights array. Reason: We are using a loop from max(weights[]) to sum(weights[]) to check all possible weights. Inside the loop, we are calling findDays() function for each weight. Now, inside the findDays() function, we are using a loop that runs for N times.
+//Space Complexity: O(1) as we are not using any extra space to solve this problem.
+
+//Optimal Approach(Using Binary Search): 
+
+import java.util.*;
+
+public class CapacityToShipPackages {
+    public static int findDays(int[] weights, int cap) {
+        int days = 1; //First day.
+        int load = 0;
+        int n = weights.length; //size of array.
+        for (int i = 0; i < n; i++) {
+            if (load + weights[i] > cap) {
+                days += 1; //move to next day
+                load = weights[i]; //load the weight.
+            } else {
+                //load the weight on the same day.
+                load += weights[i];
+            }
+        }
+        return days;
+    }
+
+    public static int leastWeightCapacity(int[] weights, int d) {
+        //Find the maximum and the summation:
+        int low = Integer.MIN_VALUE, high = 0;
+        for (int i = 0; i < weights.length; i++) {
+            high += weights[i];
+            low = Math.max(low, weights[i]);
+        }
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int numberOfDays = findDays(weights, mid);
+            if (numberOfDays <= d) {
+                //eliminate right half
+                high = mid - 1;
+            } else {
+                //eliminate left half
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+    public static void main(String[] args) {
+        int[] weights = {5, 4, 5, 2, 3, 4, 5, 6};
+        int d = 5;
+        int ans = leastWeightCapacity(weights, d);
+        System.out.println("The minimum capacity should be: " + ans);
+    }
+}
+
+//Time Complexity: O(N * log(sum(weights[]) - max(weights[]) + 1)), where sum(weights[]) = summation of all the weights, max(weights[]) = maximum of all the weights, N = size of the weights array. Reason: We are applying binary search on the range [max(weights[]), sum(weights[])]. For every possible answer ‘mid’, we are calling findDays() function. Now, inside the findDays() function, we are using a loop that runs for N times.
+//Space Complexity: O(1) as we are not using any extra space to solve this problem.
