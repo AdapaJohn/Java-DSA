c388efa3a309327176291009f55bd21cf6a36f1d
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2024-06-29 19:36:07 +0530

    Allocate Minimum Number of Pages file added
    Output: The answer is: 71.

---
+++ Data Structures & Algorithms/2. Binary Search/2.Binary Search on Answers/AllocateMinimumNumberOfPages.java
@@ -0,0 +1,102 @@
+//Brute Force Approach:
+
+import java.util.*;
+
+public class AllocateMinimumNumberOfPages {
+    public static int countStudents(ArrayList<Integer> arr, int pages) {
+        int n = arr.size(); // size of array
+        int students = 1;
+        long pagesStudent = 0;
+        for (int i = 0; i < n; i++) {
+            if (pagesStudent + arr.get(i) <= pages) {
+                // add pages to current student
+                pagesStudent += arr.get(i);
+            } else {
+                // add pages to next student
+                students++;
+                pagesStudent = arr.get(i);
+            }
+        }
+        return students;
+    }
+
+    public static int findPages(ArrayList<Integer> arr, int n, int m) {
+        // book allocation impossible
+        if (m > n)
+            return -1;
+
+        int low = Collections.max(arr);
+        int high = arr.stream().mapToInt(Integer::intValue).sum();
+
+        for (int pages = low; pages <= high; pages++) {
+            if (countStudents(arr, pages) == m) {
+                return pages;
+            }
+        }
+        return low;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(25, 46, 28, 49, 24));
+        int n = 5;
+        int m = 4;
+        int ans = findPages(arr, n, m);
+        System.out.println("The answer is: " + ans);
+    }
+}
+
+//Time Complexity: O(N * (sum(arr[])-max(arr[])+1)), where N = size of the array, sum(arr[]) = sum of all array elements, max(arr[]) = maximum of all array elements.Reason: We are using a loop from max(arr[]) to sum(arr[]) to check all possible numbers of pages. Inside the loop, we are calling the countStudents() function for each number. Now, inside the countStudents() function, we are using a loop that runs for N times.
+//Space Complexity:  O(1) as we are not using any extra space to solve this problem.
+
+//Optimal Approach: 
+
+import java.util.*;
+
+public class AllocateMinimumNumberOfPages {
+    public static int countStudents(ArrayList<Integer> arr, int pages) {
+        int n = arr.size(); // size of array
+        int students = 1;
+        long pagesStudent = 0;
+        for (int i = 0; i < n; i++) {
+            if (pagesStudent + arr.get(i) <= pages) {
+                // add pages to current student
+                pagesStudent += arr.get(i);
+            } else {
+                // add pages to next student
+                students++;
+                pagesStudent = arr.get(i);
+            }
+        }
+        return students;
+    }
+
+    public static int findPages(ArrayList<Integer> arr, int n, int m) {
+        // book allocation impossible
+        if (m > n)
+            return -1;
+
+        int low = Collections.max(arr);
+        int high = arr.stream().mapToInt(Integer::intValue).sum();
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int students = countStudents(arr, mid);
+            if (students > m) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(25, 46, 28, 49, 24));
+        int n = 5;
+        int m = 4;
+        int ans = findPages(arr, n, m);
+        System.out.println("The answer is: " + ans);
+    }
+}
+
+//Time Complexity: O(N * log(sum(arr[])-max(arr[])+1)), where N = size of the array, sum(arr[]) = sum of all array elements, max(arr[]) = maximum of all array elements.Reason: We are applying binary search on [max(arr[]), sum(arr[])]. Inside the loop, we are calling the countStudents() function for the value of ‘mid’. Now, inside the countStudents() function, we are using a loop that runs for N times.
+//Space Complexity:  O(1) as we are not using any extra space to solve this problem.
