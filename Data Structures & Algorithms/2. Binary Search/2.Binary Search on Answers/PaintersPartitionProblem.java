230c1b23f7fcd7f81f5ce13cfafcf3e89fd40af0
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2024-08-04 16:33:44 +0530

    Painter's Partition Problem file added
    Output: The answer is: 60

---
+++ Data Structures & Algorithms/2. Binary Search/2.Binary Search on Answers/PaintersPartitionProblem.java
@@ -0,0 +1,94 @@
+//Brute Force Approach:
+
+import java.util.*;
+
+public class PaintersPartitionProblem {
+    public static int countPainters(ArrayList<Integer> boards, int time) {
+        int n = boards.size(); // size of array.
+        int painters = 1;
+        long boardsPainter = 0;
+        for (int i = 0; i < n; i++) {
+            if (boardsPainter + boards.get(i) <= time) {
+                // allocate board to current painter
+                boardsPainter += boards.get(i);
+            } else {
+                // allocate board to next painter
+                painters++;
+                boardsPainter = boards.get(i);
+            }
+        }
+        return painters;
+    }
+
+    public static int findLargestMinDistance(ArrayList<Integer> boards, int k) {
+        int low = Collections.max(boards);
+        int high = boards.stream().mapToInt(Integer::intValue).sum();
+
+        for (int time = low; time <= high; time++) {
+            if (countPainters(boards, time) <= k) {
+                return time;
+            }
+        }
+        return low;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> boards = new ArrayList<>(Arrays.asList(10, 20, 30, 40));
+        int k = 2;
+        int ans = findLargestMinDistance(boards, k);
+        System.out.println("The answer is: " + ans);
+    }
+}
+
+//Time Complexity: O(N * (sum(arr[])-max(arr[])+1)), where N = size of the array, sum(arr[]) = sum of all array elements, max(arr[]) = maximum of all array elements.Reason: We are using a loop from max(arr[]) to sum(arr[]) to check all possible values of time. Inside the loop, we are calling the countPainters() function for each number. Now, inside the countPainters() function, we are using a loop that runs for N times.
+//Space Complexity:  O(1) as we are not using any extra space to solve this problem.
+
+//Optimal Approach:
+
+import java.util.*;
+
+public class PaintersPartitionProblem {
+    public static int countPainters(ArrayList<Integer> boards, int time) {
+        int n = boards.size(); // size of array.
+        int painters = 1;
+        long boardsPainter = 0;
+        for (int i = 0; i < n; i++) {
+            if (boardsPainter + boards.get(i) <= time) {
+                // allocate board to current painter
+                boardsPainter += boards.get(i);
+            } else {
+                // allocate board to next painter
+                painters++;
+                boardsPainter = boards.get(i);
+            }
+        }
+        return painters;
+    }
+
+    public static int findLargestMinDistance(ArrayList<Integer> boards, int k) {
+        int low = Collections.max(boards);
+        int high = boards.stream().mapToInt(Integer::intValue).sum();
+
+        // Apply binary search:
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int painters = countPainters(boards, mid);
+            if (painters > k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> boards = new ArrayList<>(Arrays.asList(10, 20, 30, 40));
+        int k = 2;
+        int ans = findLargestMinDistance(boards, k);
+        System.out.println("The answer is: " + ans);
+    }
+}
+
+//Time Complexity: O(N * log(sum(arr[])-max(arr[])+1)), where N = size of the array, sum(arr[]) = sum of all array elements, max(arr[]) = maximum of all array elements.  Reason: We are applying binary search on [max(arr[]), sum(arr[])]. Inside the loop, we are calling the countPainters() function for the value of ‘mid’. Now, inside the countPainters() function, we are using a loop that runs for N times.
+//Space Complexity:  O(1) as we are not using any extra space to solve this problem.
