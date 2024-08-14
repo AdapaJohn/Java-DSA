d4223bbc7c7cb25401e40a7337cb1ac24c428dc0
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2024-08-14 12:05:13 +0530

    Minimise Maximum Distance between Gas Stations file added
    Output: The answer is: 0.5

---
+++ Data Structures & Algorithms/2. Binary Search/2.Binary Search on Answers/MinimiseMaximumDistanceBetweenGasStations.java
@@ -0,0 +1,158 @@
+//Brute Force Approach:
+
+import java.util.*;
+
+public class MinimiseMaximumDistanceBetweenGasStations {
+    public static double minimiseMaxDistance(int[] arr, int k) {
+        int n = arr.length; //size of array.
+        int[] howMany = new int[n - 1];
+
+        //Pick and place k gas stations:
+        for (int gasStations = 1; gasStations <= k; gasStations++) {
+            //Find the maximum section
+            //and insert the gas station:
+            double maxSection = -1;
+            int maxInd = -1;
+            for (int i = 0; i < n - 1; i++) {
+                double diff = arr[i + 1] - arr[i];
+                double sectionLength =
+                    diff / (double)(howMany[i] + 1);
+                if (sectionLength > maxSection) {
+                    maxSection = sectionLength;
+                    maxInd = i;
+                }
+            }
+            //insert the current gas station:
+            howMany[maxInd]++;
+        }
+
+        //Find the maximum distance i.e. the answer:
+        double maxAns = -1;
+        for (int i = 0; i < n - 1; i++) {
+            double diff = arr[i + 1] - arr[i];
+            double sectionLength =
+                diff / (double)(howMany[i] + 1);
+            maxAns = Math.max(maxAns, sectionLength);
+        }
+        return maxAns;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int k = 4;
+        double ans = minimiseMaxDistance(arr, k);
+        System.out.println("The answer is: " + ans);
+    }
+}
+
+//Time Complexity: O(k*n) + O(n), n = size of the given array, k = no. of gas stations to be placed.Reason: O(k*n) to insert k gas stations between the existing stations with maximum distance. Another O(n) for finding the answer i.e. the maximum distance.
+//Space Complexity: O(n-1) as we are using an array to keep track of placed gas stations.
+
+//Better Approach(Using Heap):
+
+import java.util.*;
+
+public class MinimiseMaximumDistanceBetweenGasStations {
+    public static class Pair {
+        double first;
+        int second;
+
+        Pair(double first, int second) {
+            this.first = first;
+            this.second = second;
+        }
+    }
+
+    public static double minimiseMaxDistance(int[] arr, int k) {
+        int n = arr.length; // size of array.
+        int[] howMany = new int[n - 1];
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> Double.compare(b.first, a.first));
+
+        // insert the first n-1 elements into pq
+        // with respective distance values:
+        for (int i = 0; i < n - 1; i++) {
+            pq.add(new Pair(arr[i + 1] - arr[i], i));
+        }
+
+        // Pick and place k gas stations:
+        for (int gasStations = 1; gasStations <= k; gasStations++) {
+            // Find the maximum section
+            // and insert the gas station:
+            Pair tp = pq.poll();
+            int secInd = tp.second;
+
+            // insert the current gas station:
+            howMany[secInd]++;
+
+            double inidiff = arr[secInd + 1] - arr[secInd];
+            double newSecLen = inidiff / (double) (howMany[secInd] + 1);
+            pq.add(new Pair(newSecLen, secInd));
+        }
+
+        return pq.peek().first;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int k = 4;
+        double ans = minimiseMaxDistance(arr, k);
+        System.out.println("The answer is: " + ans);
+    }
+}
+
+//Time Complexity: O(nlogn + klogn),  n = size of the given array, k = no. of gas stations to be placed. Reason: Insert operation of priority queue takes logn time complexity. O(nlogn) for inserting all the indices with distance values and O(klogn) for placing the gas stations.
+//Space Complexity: O(n-1)+O(n-1) Reason: The first O(n-1) is for the array to keep track of placed gas stations and the second one is for the priority queue.
+
+//Optimal Approach(Using Binary Search):
+
+import java.util.*;
+
+public class MinimiseMaximumDistanceBetweenGasStations {
+    public static int numberOfGasStationsRequired(double dist, int[] arr) {
+        int n = arr.length; // size of the array
+        int cnt = 0;
+        for (int i = 1; i < n; i++) {
+            int numberInBetween = (int)((arr[i] - arr[i - 1]) / dist);
+            if ((arr[i] - arr[i - 1]) == (dist * numberInBetween)) {
+                numberInBetween--;
+            }
+            cnt += numberInBetween;
+        }
+        return cnt;
+    }
+
+    public static double minimiseMaxDistance(int[] arr, int k) {
+        int n = arr.length; // size of the array
+        double low = 0;
+        double high = 0;
+
+        //Find the maximum distance:
+        for (int i = 0; i < n - 1; i++) {
+            high = Math.max(high, (double)(arr[i + 1] - arr[i]));
+        }
+
+        //Apply Binary search:
+        double diff = 1e-6 ;
+        while (high - low > diff) {
+            double mid = (low + high) / (2.0);
+            int cnt = numberOfGasStationsRequired(mid, arr);
+            if (cnt > k) {
+                low = mid;
+            } else {
+                high = mid;
+            }
+        }
+        return high;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int k = 4;
+        double ans = minimiseMaxDistance(arr, k);
+        System.out.println("The answer is: " + ans);
+    }
+}
+
+
+//Time Complexity: O(n*log(Len)) + O(n), n = size of the given array, Len = length of the answer space. Reason: We are applying binary search on the answer space. For every possible answer, we are calling the function numberOfGasStationsRequired() that takes O(n) time complexity. And another O(n) for finding the maximum distance initially.
+//Space Complexity: O(1) as we are using no extra space to solve this problem.
