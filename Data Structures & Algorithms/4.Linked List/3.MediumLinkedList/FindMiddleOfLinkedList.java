1e2b0c3b680bf1dbb67aaf12951c86f9206e6b35
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2025-02-14 22:11:15 +0530

    Find middle element in a Linked List
    Output: The middle node value is: 3

---
+++ Data Structures & Algorithms/4.Linked List/3.MediumLinkedList/FindMiddleOfLinkedList.java
@@ -0,0 +1,66 @@
+                                
+import java.util.*;
+
+// Node class represents a node in a linked list
+class Node {
+    // Data stored in the node
+    int data; 
+     // Pointer to the next node in the list
+    Node next;     
+
+    // Constructor with both data
+    // and next node as parameters
+    Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    // Constructor with only data as
+    // a parameter, sets next to null
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class FindMiddleOfLinkedList {
+    
+    static Node findMiddle(Node head) {
+        // Initialize the slow pointer to the head.
+        Node slow = head; 
+        
+        // Initialize the fast pointer to the head.
+        Node fast = head;   
+
+        // Traverse the linked list using
+        // the Tortoise and Hare algorithm.
+        while (fast != null && fast.next != null && slow != null) {
+            // Move fast two steps.
+            fast = fast.next.next;  
+            // Move slow one step.
+            slow = slow.next;        
+        }
+        // Return the slow pointer,
+        // which is now at the middle node.
+        return slow;  
+    }
+
+
+    public static void main(String[] args) {
+        // Creating a sample linked list:
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+
+        // Find the middle node
+        Node middleNode = findMiddle(head);
+
+        // Display the value of the middle node
+        System.out.println("The middle node value is: " + middleNode.data);
+    }
+}
+
+//Time Complexity: O(N/2) The algorithm requires the 'fast' pointer to reach the end of the list which it does after approximately N/2 iterations (where N is the total number of nodes). Therefore, the maximum number of iterations needed to find the middle node is proportional to the number of nodes in the list, making the time complexity linear, or O(N/2) ~ O(N).
+//Space Complexity : O(1) There is constant space complexity because it uses a constant amount of extra space regardless of the size of the linked list. We only use a few variables to keep track of the middle position and traverse the list, and the memory required for these variables does not depend on the size of the list.
