195c4ed048f818e0191043d3bc80433f03ba9712
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2025-03-09 21:31:32 +0530

    Implement Stack using single Queue
    Output:
    Top of the stack: 1
    Size of the stack before removing element: 4
    The deleted element is: 1
    Top of the stack after removing element: 4
    Size of the stack after removing element: 3

---
+++ Data Structures & Algorithms/6.Stacks & Queues/1.Basic/ImplementStackUsingSingleQueue.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class ImplementStackUsingSingleQueue {
+    public static void main(String[] args) {
+        stack s = new stack();
+        s.push(3);
+        s.push(2);
+        s.push(4);
+        s.push(1);
+        System.out.println("Top of the stack: " + s.top());
+        System.out.println("Size of the stack before removing element: " + s.size());
+        System.out.println("The deleted element is: " + s.pop());
+        System.out.println("Top of the stack after removing element: " + s.top());
+        System.out.println("Size of the stack after removing element: " + s.size());
+    }
+
+}
+class stack {
+    Queue < Integer > q = new LinkedList < > ();
+    void push(int x) {
+        q.add(x);
+        for (int i = 0; i < q.size() - 1; i++) {
+            q.add(q.remove());
+        }
+    }
+    int pop() {
+        return q.remove();
+    }
+    int top() {
+        return q.peek();
+    }
+    int size() {
+        return q.size();
+    }
+}
+
+//Time Complexity: O(N)
+//Space Complexity: O(N)
