cf00a7d02f5b603b3c33fe5bfaf9f69f830e1846
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2025-03-13 23:10:35 +0530

    Infix to Postfix
    Output:
    Infix expression: (p+q)*(m-n)
    Prefix expression: pq+mn-*

---
+++ Data Structures & Algorithms/6.Stacks & Queues/2.Lec 2: Prefix, Infix, PostFix Conversion Problems/InfixToPostfix.java
@@ -0,0 +1,87 @@
+import java.util.Stack;
+
+class InfixToPostfix {
+
+  // A utility function to return
+  // precedence of a given operator
+  // Higher returned value means
+  // higher precedence
+  static int Prec(char ch) {
+    switch (ch) {
+    case '+':
+    case '-':
+      return 1;
+
+    case '*':
+    case '/':
+      return 2;
+
+    case '^':
+      return 3;
+    }
+    return -1;
+  }
+
+  // The main method that converts
+  // given infix expression
+  // to postfix expression.
+  static String infixToPostfix(String exp) {
+    // initializing empty String for result
+    String result = new String("");
+
+    // initializing empty stack
+    Stack < Character > stack = new Stack < > ();
+
+    for (int i = 0; i < exp.length(); ++i) {
+      char c = exp.charAt(i);
+
+      // If the scanned character is an
+      // operand, add it to output.
+      if (Character.isLetterOrDigit(c))
+        result += c;
+
+      // If the scanned character is an '(',
+      // push it to the stack.
+      else if (c == '(')
+        stack.push(c);
+
+      // If the scanned character is an ')',
+      // pop and output from the stack
+      // until an '(' is encountered.
+      else if (c == ')') {
+        while (!stack.isEmpty() &&
+          stack.peek() != '(')
+          result += stack.pop();
+
+        stack.pop();
+      } else // an operator is encountered
+      {
+        while (!stack.isEmpty() && Prec(c) <=
+          Prec(stack.peek())) {
+
+          result += stack.pop();
+        }
+        stack.push(c);
+      }
+
+    }
+
+    // pop all the operators from the stack
+    while (!stack.isEmpty()) {
+      if (stack.peek() == '(')
+        return "Invalid Expression";
+      result += stack.pop();
+    }
+    return result;
+  }
+
+  // Driver method
+  public static void main(String[] args) {
+    String exp = "(p+q)*(m-n)";
+    System.out.println("Infix expression: " + exp);
+    System.out.println("Prefix expression: " + infixToPostfix(exp));
+  }
+}
+
+//Time Complexity: O(N)
+//Space Complexity: O(N) for using the stack
