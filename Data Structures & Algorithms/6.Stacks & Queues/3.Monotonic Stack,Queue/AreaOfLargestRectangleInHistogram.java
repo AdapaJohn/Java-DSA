0463fbc0974beaad155d7dc5d31de880e0fd6d28
Adapa John <132468205+AdapaJohn@users.noreply.github.com>
2025-03-14 22:34:17 +0530

    Area of largest rectangle in Histogram
    Output: The largest area in the histogram is 10

---
+++ Data Structures & Algorithms/6.Stacks & Queues/3.Monotonic Stack,Queue/AreaOfLargestRectangleInHistogram.java
@@ -0,0 +1,32 @@
+//https://takeuforward.org/data-structure/area-of-largest-rectangle-in-histogram/
+
+import java.util.*;
+public class AreaOfLargestRectangleInHistogram {
+    static int largestRectangleArea(int histo[]) {
+        Stack < Integer > st = new Stack < > ();
+        int maxA = 0;
+        int n = histo.length;
+        for (int i = 0; i <= n; i++) {
+            while (!st.empty() && (i == n || histo[st.peek()] >= histo[i])) {
+                int height = histo[st.peek()];
+                st.pop();
+                int width;
+                if (st.empty())
+                    width = i;
+                else
+                    width = i - st.peek() - 1;
+                maxA = Math.max(maxA, width * height);
+            }
+            st.push(i);
+        }
+        return maxA;
+    }
+
+    public static void main(String args[]) {
+        int histo[] = {3, 1, 5, 6, 2, 3};
+        System.out.println("The largest area in the histogram is " + largestRectangleArea(histo));
+    }
+}
+
+//Time Complexity: O( N ) + O (N)
+//Space Complexity: O(N)
